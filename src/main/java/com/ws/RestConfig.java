4c008b418b0b58276baa7e9cc4be49b28edc701d
Aioshino <jarbues.241ds04@istta.edu.pe>
2026-05-14 00:25:40 -0500

    Delete src/main directory

--- src/main/java/com/ws/RestConfig.java
+++
@@ -1,6 +0,0 @@
-package com.ws;
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("api") // Sin la barra inicial aquí, solo "api"
-public class RestConfig extends Application { }
