de4c8919939a0bda7356a973dee5ff1caea26f05
Aioshino <jarbues.241ds04@istta.edu.pe>
2026-05-14 09:35:51 -0500

    Update HolaMundo.java

--- src/main/java/com/ws/HolaMundo.java
+++ src/main/java/com/ws/HolaMundo.java
@@ -1,4 +1,5 @@
 package com.ws;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -6,9 +7,27 @@ import jakarta.ws.rs.core.MediaType;
 
 @Path("/saludo")
 public class HolaMundo {
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String decirHola() {
-        return "¡Hola Mundo desde Java REST!";
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public Saludo decirHola() {
+        return new Saludo("¡Hola Mundo desde Java REST!");
+    }
+
+    // Clase interna para estructurar el JSON
+    public static class Saludo {
+        private String mensaje;
+
+        public Saludo(String mensaje) {
+            this.mensaje = mensaje;
+        }
+
+        public String getMensaje() {
+            return mensaje;
+        }
+
+        public void setMensaje(String mensaje) {
+            this.mensaje = mensaje;
+        }
     }
 }
